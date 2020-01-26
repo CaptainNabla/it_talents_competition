c08846623effda74554d5b09176b04961e5e9e29
unknown <>
2020-01-26 07:41:03 +0100

    IMP: test for reorder_date

--- tests/test_preprocessing.py
+++ tests/test_preprocessing.py
@@ -1,7 +1,7 @@
 import pytest
 import pandas as pd
 
-from data.preprocessing import date_to_numeric
+from data.preprocessing import date_to_numeric, reorder_date
 
 
 def test_date_to_numeric_ignoreInput():
@@ -15,5 +15,13 @@ def test_date_to_numeric_returnTranslation():
     input_row = "01 Mai"
     expected = "01.05"
     actual = date_to_numeric(input_row)
+
+    assert expected == actual
+
+
+def test_reorder_date():
+    input_date = "2000-04-01"
+    expected = "2000-01-04"
+    actual = reorder_date(input_date)
 
     assert expected == actual
