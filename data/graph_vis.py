7094af698f0d23c39bcf5d60b83d41fe77636516
Laksan Nathan <laksan.nathan@sdx-ag.de>
2020-01-31 22:20:22 +0100

    Finish Graph

--- data/graph_vis.py
+++ data/graph_vis.py
@@ -1,34 +1,41 @@
 # %%
 import numpy as np
 import pandas as pd
+
 import matplotlib.pyplot as plt
-
+import itertools
 import networkx as nx
+from networkx.algorithms import clique
+from random import randint
 
-G = nx.Graph()
 
-data = pd.read_csv("cleaned_data/races_cleaned.csv")
+def draw_graph(node_list, title):
+
+    G = nx.DiGraph()
+    G.add_edges_from(node_list)
+
+    nx.draw(G,
+            cmap=plt.get_cmap('jet'),
+            node_color="lightgrey",
+            with_labels=True)
+    
+    plt.title(title)
+    plt.show()
+
+
+data = pd.read_csv("data/cleaned_data/races_cleaned.csv")
 data.dropna(inplace=True)
-
-#data['win_weight'] = [-1 for c, w in zip(data['challenger'], data['winner'])
-
-data = data.head(500)
-
 # %%
 
-G = nx.DiGraph()
-G.add_edges_from([(c, o) for c,o in zip(data.challenger, data.opponent)])
+for _ in range(10):
+    random_racer = data.challenger.values[randint(0, len(data)-1)]
 
-# val_map = {'A': 1.0,
-#            'D': 0.5714285714285714,
-#            'H': 0.0}
+    random_racer_df = data[(data['challenger'] == random_racer)
+                        | (data['opponent'] == random_racer)]
 
-# values = [val_map.get(node, 0.25) for node in G.nodes()]
-
-nx.draw(G, cmap = plt.get_cmap('jet'), node_color = "grey", with_labels=True)
-plt.show()
-
+    draw_graph([(a, b) for a, b in zip(random_racer_df.challenger,
+                                       random_racer_df.opponent)], "")
 # %%
-data
+
 
 # %%
---
+++ visualization/feature_engineering.py
@@ -0,0 +1,59 @@
+# %%
+import numpy as np
+import pandas as pd
+import itertools
+
+from data.transformation import compute_elo, extract_player_info
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set_style("whitegrid")
+
+
+# %%
+data = pd.read_csv("data/cleaned_data/races_cleaned.csv")
+data[["race_created", "race_driven"]] = data[["race_created", "race_driven"]].astype("datetime64")
+
+# %%
+# Extract player id
+opponents = data.opponent.unique()
+opponents.sort()
+challengers = data.challenger.unique()
+challengers.sort()
+
+player_ids = list(set(list(opponents) + list(challengers)))
+
+
+# %%
+# Create Elo time series
+elo_start = 1500  # which value??????????
+elo_df = pd.DataFrame(columns=["date"]+["player_" + str(player_id) for player_id in player_ids])
+
+# 1 day less for initialization
+elo_df["date"] = pd.date_range(start=data["race_created"].min()-pd.to_timedelta("1D"), end=data["race_created"].max(), freq="D")
+elo_df.loc[0,1:] = elo_start
+
+# %%
+import time
+from tqdm import tqdm
+start = time.time()
+df_grouped = data.loc[:2000].dropna().groupby("race_created")
+current_index = 1 # adjust for "ghost row"!
+for name, group in tqdm(df_grouped):
+    elo_df.iloc[current_index] = elo_df.iloc[current_index-1:current_index+1].fillna(method="ffill").iloc[1]
+    for _, row in group.iterrows():
+        challenger_id, opponent_id, winner = extract_player_info(row)
+        challenger_elo = elo_df.iloc[current_index, player_ids.index(challenger_id)]
+        opponent_elo = elo_df.iloc[current_index, player_ids.index(opponent_id)]
+        challenger_elo_new, opponent_elo_new = compute_elo(elo_challenger=challenger_elo,
+                                                           elo_opponent=opponent_elo,
+                                                           winner=winner)
+        elo_df.iloc[current_index, player_ids.index(challenger_id)] = challenger_elo_new
+        elo_df.iloc[current_index, player_ids.index(opponent_id)] = opponent_elo_new
+    current_index += 1
+    # ???
+end= time.time()
+print(end-start)
+
+
+# %%
